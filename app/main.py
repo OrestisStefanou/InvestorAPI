8c7776aca985c4a241f8e9aa24c87bfe255f0c33
OrestisStefanou <orestaras1997@outlook.com>
2022-12-19 23:19:10 +0200

    Closing db connection on shutdown event

--- app/main.py
+++ app/main.py
@@ -2,8 +2,13 @@ from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 
 from app.graphql.api import schema
+from app import dependencies
 
 app = FastAPI()
 graphql_app = GraphQLRouter(schema)
 
 app.include_router(graphql_app, prefix="/graphql")
+
+@app.on_event("shutdown")
+def shutdown_event():
+    dependencies.db_conn.close()
