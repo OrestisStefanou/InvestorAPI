e6661577fdef0da2a33c811fb350963a544e5f30
OrestisStefanou <orestaras1997@outlook.com>
2023-05-01 13:04:27 +0300

    Updated index time series data

---
+++ app/scripts/scrape_and_store_ibd_data.py
@@ -0,0 +1,81 @@
+import logging
+import time
+
+from app.services.bottom_comp_stocks import BottomCompositeStocksService
+from app.services.dividend_leaders import DividendLeadersService
+from app.services.leaders_index import SmallMidCapLeadersIndexService, LargeMidCapLeadersIndexService
+from app.services.reit_leaders import ReitLeadersService
+from app.services.stocks_with_sector import StocksWithSectorService
+from app.services.tech_leaders_stocks import TechLeadersStocksService
+from app.services.top_comp_stocks import TopCompositeStocksService
+from app.services.top_low_priced_stocks import TopLowPricedStocksService
+from app.services.utility_leaders import UtilityLeadersService
+
+async def scrape_and_store_data(day: int, month: int, year: int):
+    try:
+        await BottomCompositeStocksService.scrape_and_store_bottom_200_comp_stocks_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store bottom composite stocks:", err)
+
+    time.sleep(2)   # We sleep to don't send all the requests at the same time
+
+    try:
+        await DividendLeadersService.scrape_and_store_dividend_leaders_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store dividend leaders:", err)
+
+    time.sleep(2)
+
+    try:
+        await SmallMidCapLeadersIndexService.scrape_and_store_leaders_index_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store small mid cap leaders index:", err)
+
+    time.sleep(2)
+
+    try:
+        await LargeMidCapLeadersIndexService.scrape_and_store_leaders_index_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store large mid cap leaders index:", err)
+
+    time.sleep(2)
+
+    try:
+        await ReitLeadersService.scrape_and_store_reit_leaders_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store reit leaders:", err)
+
+    time.sleep(2)
+
+    try:
+        await StocksWithSectorService.scrape_and_store_stocks_with_sector_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store stocks with sector:", err)
+
+    time.sleep(2)
+
+    try:
+        await TechLeadersStocksService.scrape_and_store_tech_leaders_stocks_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store tech leaders:", err)
+
+    time.sleep(2)
+
+    try:
+        await TopCompositeStocksService.scrape_and_store_top_200_comp_stocks_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store top composite stocks:", err)
+
+    time.sleep(2)
+
+    try:
+        await TopLowPricedStocksService.scrape_and_store_top_low_priced_stocks_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store top low priced stocks:", err)
+
+    time.sleep(2)
+
+    try:
+        await UtilityLeadersService.scrape_and_store_utility_leaders_for_date(day, month, year)
+    except Exception as err:
+        logging.error("Failed to scrape and store utility leaders:", err)
---
+++ app/scripts/scrape_and_store_world_indices.py
@@ -0,0 +1,15 @@
+import time
+
+from app.domain.world_index import WorldIndex
+
+from app.services.time_series import TimeSeriesService
+
+async def scrape_and_store_world_indices_time_series():
+    await TimeSeriesService.scrape_and_store_index_time_series(WorldIndex.S_P_500)
+    time.sleep(60)
+    await TimeSeriesService.scrape_and_store_index_time_series(WorldIndex.Dow_Jones_Ind_Avg)
+    time.sleep(60)
+    await TimeSeriesService.scrape_and_store_index_time_series(WorldIndex.Nasdaq_Composite)
+    time.sleep(60)
+    await TimeSeriesService.scrape_and_store_index_time_series(WorldIndex.Nyse_Composite)
+    
