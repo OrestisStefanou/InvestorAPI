227b35917788b0189e8e07c566ac2978999dfa65
OrestisStefanou <orestaras1997@outlook.com>
2023-05-20 10:37:40 +0300

    Updated economic indicators

---
+++ app/scripts/scrape_and_store_economic_indicators.py
@@ -0,0 +1,11 @@
+import time
+
+from app.services.time_series import TimeSeriesService
+from app.domain.economic_indicator import EconomicIndicator
+
+async def scrape_and_store_economic_indicators_time_series():
+    await TimeSeriesService.scrape_and_store_economic_indicator_time_series(EconomicIndicator.Interest_Rate)
+    time.sleep(1)
+    await TimeSeriesService.scrape_and_store_economic_indicator_time_series(EconomicIndicator.Treasury_Yield)
+    time.sleep(1)
+    await TimeSeriesService.scrape_and_store_economic_indicator_time_series(EconomicIndicator.Unemployment)
