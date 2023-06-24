cb23a8236140475f1707ecbc644100cbd72c6c2a
OrestisStefanou <orestaras1997@outlook.com>
2023-06-24 21:49:13 +0300

    WIP: Using alpha vantage instead of yf to get company overview data

--- app/domain/stock_overview.py
+++ app/domain/stock_overview.py
@@ -15,8 +15,7 @@ class StockOverview:
     book_value: Optional[float] = None
     divided_per_share: Optional[float] = None
     dividend_yield: Optional[float] = None
-    trailing_eps: Optional[float] = None
-    forward_eps: Optional[float] = None
+    eps: Optional[float] = None
     revenue_per_share: Optional[float] = None
     profit_margins: Optional[float] = None
     operating_margins: Optional[float] = None
@@ -26,7 +25,9 @@ class StockOverview:
     gross_profit: Optional[float] = None
     earnings_growth: Optional[float] = None
     revenue_growth: Optional[float] = None
-    target_high_price: Optional[float] = None
-    target_low_price: Optional[float] = None
-    target_mean_price: Optional[float] = None
-    target_median_price: Optional[float] = None
+    target_price: Optional[float] = None
+    beta: Optional[float] = None
+    price_to_sales_ratio: Optional[float] = None
+    price_to_book_ratio: Optional[float] = None
+    ev_to_revenue: Optional[float] = None
+    ev_to_ebitda: Optional[float] = None
--- app/scripts/scrape_and_store_stock_overview.py
+++ app/scripts/scrape_and_store_stock_overview.py
@@ -1,2 +1,69 @@
-import yfinance as yf
+import time
+import asyncio
 
+from app import dependencies
+from app.repos.stock_overview_repo import StockOverviewRepo
+from app.http.alpha_vantage_client import AlphaVantageClient
+from app.domain.stock_overview import StockOverview
+from app.domain.sector import Sector
+from app.domain.date import Date
+
+DAY_LIMIT = 500
+MINUTE_LIMIT = 5
+
+def try_convert_to_float(string_value):
+    try:
+        float_value = float(string_value)
+        return float_value
+    except ValueError:
+        return None
+
+
+with dependencies.get_db_conn() as conn:
+    # Modify the query to fetch all the latest stock symbols that we have
+    query = '''
+        SELECT DISTINCT symbol, sector_name FROM stocks_with_sector WHERE sector_name = 'CHIPS'
+    '''
+    result = conn.execute(query)
+
+    stock_overview_repo = StockOverviewRepo()
+    av_client = AlphaVantageClient()
+
+    for row in result:
+        symbol = row[0]
+        sector = Sector(row[1])
+        
+        company_info = asyncio.run(av_client.get_company_overview(symbol))
+        stock_overview = StockOverview(
+            symbol=symbol,
+            sector=sector,
+            market_cap=try_convert_to_float(company_info.get('MarketCapitalization')),
+            ebitda=try_convert_to_float(company_info.get('EBITDA')),
+            forward_pe_ratio=try_convert_to_float(company_info.get('ForwardPE')),
+            trailing_pe_ratio=try_convert_to_float(company_info.get('TrailingPE')),
+            peg_ratio=try_convert_to_float(company_info.get('PEGRatio')),
+            book_value=try_convert_to_float(company_info.get('BookValue')),
+            divided_per_share=try_convert_to_float(company_info.get('DividendPerShare')),
+            dividend_yield=try_convert_to_float(company_info.get('DividendYield')),
+            eps=try_convert_to_float(company_info.get('EPS')),
+            revenue_per_share=try_convert_to_float(company_info.get('RevenuePerShareTTM')),
+            profit_margins=try_convert_to_float(company_info.get('ProfitMargin')),
+            operating_margins=try_convert_to_float(company_info.get('OperatingMarginTTM')),
+            return_on_assets=try_convert_to_float(company_info.get('ReturnOnAssetsTTM')),
+            return_on_equity=try_convert_to_float(company_info.get('ReturnOnEquityTTM')),
+            revenue=try_convert_to_float(company_info.get('RevenueTTM')),
+            gross_profit=try_convert_to_float(company_info.get('GrossProfitTTM')),
+            earnings_growth=try_convert_to_float(company_info.get('QuarterlyEarningsGrowthYOY')),
+            revenue_growth=try_convert_to_float(company_info.get('QuarterlyRevenueGrowthYOY')),
+            target_price=try_convert_to_float(company_info.get('AnalystTargetPrice')),
+            beta=try_convert_to_float(company_info.get('Beta')),
+            price_to_sales_ratio=try_convert_to_float(company_info.get('PriceToSalesRatioTTM')),
+            price_to_book_ratio=try_convert_to_float(company_info.get('PriceToBookRatio'))
+        )
+
+        stock_overview_repo.add_stock_overview_for_date(
+            date=Date(day=24, month=6, year=2023),
+            stock_overview=stock_overview
+        )
+
+        time.sleep(2)
