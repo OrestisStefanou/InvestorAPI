d3335d7ebb323211b7a783707f7e4d647c3ebf40
OrestisStefanou <orestaras1997@outlook.com>
2023-08-26 11:31:32 +0300

    Updated globla commodities index and inflation time series

--- app/scripts/scrape_and_store_economic_indicators.py
+++ app/scripts/scrape_and_store_economic_indicators.py
@@ -9,3 +9,7 @@ async def scrape_and_store_economic_indicators_time_series():
     await TimeSeriesService.scrape_and_store_economic_indicator_time_series(EconomicIndicator.Treasury_Yield)
     time.sleep(1)
     await TimeSeriesService.scrape_and_store_economic_indicator_time_series(EconomicIndicator.Unemployment)
+    time.sleep(1)
+    await TimeSeriesService.scrape_and_store_economic_indicator_time_series(EconomicIndicator.Global_Commodities_Index)
+    time.sleep(1)
+    await TimeSeriesService.scrape_and_store_economic_indicator_time_series(EconomicIndicator.Inflation)
