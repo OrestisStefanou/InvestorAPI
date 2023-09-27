4600605e172d2d2ab1f76fc1e2745920f9ae1124
Orestis Stefanou <o.stefanou@withplum.com>
2023-09-27 18:14:22 +0300

    Errored symbols

--- app/scripts/update_stock_data.py
+++ app/scripts/update_stock_data.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from app.scripts.scrape_and_store_fundamental_data import fetch_and_store_fundamental_data_for_symbol
 from app.scripts.scrape_and_store_stock_time_series import fetch_and_store_stock_time_series
+from app.repos.balance_sheet_repo import BalanceSheetRepo
 
 
 REQUESTS_PER_MINUTE_LIMIT = 30  # Provider limitation
@@ -26,8 +27,19 @@ def update_stock_data():
     errored_symbols = ['NESR','BBIG', 'TKO', 'CYXTQ',]
 
     for symbol in get_symbols():
-        if symbol in ['NESR','BBIG', 'TKO', 'CYXTQ',     'JEF', 'CUK']:    # 'NESR','BBIG', 'TKO' IS PROBLEMATIC THE REST ALREADY FETCHED
+        if symbol in ['NESR','BBIG', 'TKO', 'CYXTQ',]:
             continue
+        
+        balance_sheet_repo = BalanceSheetRepo()
+        balance_sheets = balance_sheet_repo.get_balance_sheets_for_symbol(symbol)
+        if balance_sheets:
+            try:
+                if asyncio.run(fetch_and_store_stock_time_series(symbol)):
+                    api_calls_count += 1
+            except Exception:
+                pass
+            continue
+
 
         if api_calls_count + 4 > REQUESTS_PER_MINUTE_LIMIT:
             time.sleep(65)
@@ -45,7 +57,8 @@ def update_stock_data():
                 api_calls_count += 1
         except Exception as err:
             errored_symbols.append(symbol)
-            print('Failed to fetch data for:', symbol)
+            api_calls_count += 1
+            print(f'Failed to fetch data for: {symbol} with error : {str(err)}')
     
     with open('errored_symbols.pkl', 'wb') as file:
         pickle.dump(errored_symbols, file)
