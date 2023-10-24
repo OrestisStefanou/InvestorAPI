627152ed807477a20ca718e9aa017006cddfd7d4
OrestisStefanou <orestaras1997@outlook.com>
2023-10-24 12:22:06 +0200

    Added util function to split data

--- analytics/machine_learning/price_prediction_with_fundamentals/utils.py
+++ analytics/machine_learning/price_prediction_with_fundamentals/utils.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+import datetime as dt
 import sqlite3
 
 import pandas as pd
@@ -8,9 +10,32 @@ def get_dataset(
 ) -> pd.DataFrame:
     if db_conn is None:
         db_conn = sqlite3.connect('/Users/orestis/MyProjects/InvestorAPI/app/database/ibd.db')
+    
     query = "SELECT * FROM price_prediction_dataset ORDER BY DATE(fiscal_date_ending)"
 
     stocks_df = pd.read_sql(query, db_conn)
     db_conn.close()
 
     return stocks_df
+
+
+def split_data_to_train_and_test(
+    df: pd.DataFrame,
+    cutoff_date: dt.datetime,
+    cutoff_date_column_name: str = "fiscal_date_ending"
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Returns (train_set_df, test_set_df)
+    """
+    df['DateColumn'] = pd.to_datetime(df[cutoff_date_column_name])
+    # Split the data into train and test based on the cutoff date
+    train_set = df[df['DateColumn'] < cutoff_date].copy()
+    test_set = df[df['DateColumn'] >= cutoff_date].copy()
+
+    train_set.drop(['DateColumn',], axis=1, inplace=True)
+    test_set.drop(['DateColumn',], axis=1, inplace=True)
+    
+    train_set = train_set.reset_index(drop=True)
+    test_set = test_set.reset_index(drop=True)
+
+    return train_set, test_set 
