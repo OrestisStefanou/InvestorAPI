a9ee525dd34eb28a4a50b262c6d7153b9a166a40
OrestisStefanou <orestaras1997@outlook.com>
2023-12-28 11:38:08 +0200

    Trying to tidy functions

--- analytics/machine_learning/price_prediction_with_fundamentals/utils.py
+++ analytics/machine_learning/price_prediction_with_fundamentals/utils.py
@@ -1,6 +1,5 @@
 from typing import (
     Tuple,
-    Optional,
     Dict,
     List
 )
@@ -9,143 +8,79 @@ import sqlite3
 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import (
-    OneHotEncoder,
-    MinMaxScaler,
-    StandardScaler
-)
 from sklearn.metrics import mean_absolute_percentage_error
 
-from analytics.machine_learning.utils import preprocessing
+def get_stock_fundamental_df(symbol: str, conn: sqlite3.Connection) -> pd.DataFrame:
+    query = f'''
+        SELECT 
+            income_statement.total_revenue,
+            income_statement.gross_profit,
+            income_statement.operating_income,
+            income_statement.net_income,
+            income_statement.ebitda,
+            income_statement.net_interest_income,
 
-
-def get_features(
-    db_conn = None,
-    features_categories: List[str] = None,
-    target: str = 'price_pct_change_next_three_months'
-) -> pd.DataFrame:
-    """
-    Params:
-        - db_conn: Database connection object
-        - features_cagories: A list with the categories of features to keep.
-            Available feature categories: 'arctan_ratio', 'arctan_pct_change', 'pct_change'
-        - target: Name of the target column
-            Available targets: 'avg_next_three_months_price', 'price_pct_change_next_three_months'
-    """
-    if db_conn is None:
-        db_conn = sqlite3.connect('/Users/orestis/MyProjects/InvestorAPI/app/database/ibd.db')
-
-    query = '''
-        SELECT * 
-        FROM price_prediction_features
-        WHERE DATE(fiscal_date_ending) <= date('now', '-3 months')
-        ORDER BY DATE(fiscal_date_ending)
+            balance_sheet.total_assets,
+            balance_sheet.total_liabilities,
+            balance_sheet.total_shareholder_equity,
+            balance_sheet.total_current_assets,
+            balance_sheet.total_current_liabilities,
+            balance_sheet.cash_and_cash_equivalents_at_carrying_value,
+            balance_sheet.long_term_debt,
+            balance_sheet.current_net_receivables,
+            balance_sheet.inventory,
+            balance_sheet.property_plant_equipment,
+            
+            cash_flow.operating_cashflow,
+            cash_flow.capital_expenditures,
+            cash_flow.cashflow_from_investment,
+            cash_flow.cashflow_from_financing,
+            cash_flow.dividend_payout,
+            cash_flow.proceeds_from_issuance_of_long_term_debt_and_capital_securities_net,
+            cash_flow.payments_for_repurchase_of_equity,
+            
+            stock_overview.sector,
+            stock_overview.symbol,
+            income_statement.fiscal_date_ending
+        FROM income_statement
+        INNER JOIN balance_sheet
+            ON income_statement.fiscal_date_ending = balance_sheet.fiscal_date_ending  AND balance_sheet.symbol = '{symbol}'
+        INNER JOIN cash_flow
+            ON income_statement.fiscal_date_ending = cash_flow.fiscal_date_ending  AND cash_flow.symbol = '{symbol}'
+        INNER JOIN stock_overview
+            ON income_statement.symbol = stock_overview.symbol  AND stock_overview.symbol = '{symbol}'
+        WHERE income_statement.symbol = '{symbol}'
+        ORDER BY DATE(income_statement.fiscal_date_ending)
     '''
+    stock_df = pd.read_sql(query, conn)
+    
+    # Drop columns with duplicated names
+    stock_df = stock_df.loc[:, ~stock_df.columns.duplicated()]
 
-    features_df = pd.read_sql(query, db_conn)
-    db_conn.close()
+    # List of columns to convert to float
+    columns_to_convert = stock_df.columns.difference(
+        ['symbol', 'fiscal_date_ending', 'sector']
+    )
 
-    # Drop rows that contain null values
-    columns_with_null = features_df.columns[features_df.isna().any()].tolist()
-    features_df.dropna(subset=columns_with_null, inplace=True)
-    features_df.reset_index(inplace=True, drop=True)
+    # Convert selected columns to float
+    stock_df[columns_to_convert] = stock_df[columns_to_convert].astype(float)
 
-    if features_categories:
-        valid_feature_categories = ['arctan_ratio', 'arctan_pct_change', 'pct_change']
-        invalid_categories = [category for category in features_categories if category not in valid_feature_categories]
-        if invalid_categories:
-            raise ValueError(f"Available categories are: {', '.join(valid_feature_categories)}")
+    # We fill NaN values with a small value because later we perform feature
+    # engineering to calculate pct change and ratios and we can't divide witrh zero
+    stock_df[columns_to_convert] = stock_df[columns_to_convert].fillna(0.01)
+    stock_df[columns_to_convert] = stock_df[columns_to_convert].replace(0, 0.01)
 
-        columns_to_keep = [
-            'symbol', 
-            'sector', 
-            'fiscal_date_ending', 
-            'avg_interest_rate', 
-            'avg_treasury_yield',
-            'avg_unemployment_rate',
-            'avg_global_commodities_index_value',
-            'avg_oil_price',
-            'avg_natural_gas_price',
-            'inflation',
-            'price_volatility_three_months',
-        ]
+    # Feature engineering
+    for column in columns_to_convert:
+        # arctan percentage change
+        log_pct_change_column_name = f'{column}_arctan_pct_change'
+        stock_df[log_pct_change_column_name] = np.arctan(stock_df[column].pct_change())
 
-        if target == 'price_pct_change_next_three_months':
-            columns_to_keep = columns_to_keep + ['price_pct_change_three_months', 'price_pct_change_next_three_months']
-        else:
-            columns_to_keep = columns_to_keep + ['avg_three_months_price', 'avg_next_three_months_price']
+        # Plain percentage change
+        pct_change_column_name = f'{column}_pct_change'
+        stock_df[pct_change_column_name] = stock_df[column].pct_change()
 
-        for col_name in features_df.columns:
-            for category in features_categories:
-                if str(col_name).endswith(f'_{category}'):
-                    if category == 'pct_change' and str(col_name).endswith(f'_arctan_pct_change'):
-                        continue
-
-                    columns_to_keep.append(col_name)
-
-        return features_df[columns_to_keep]
-
-    return features_df
-
-
-def get_dataset(
-    db_conn = None,
-) -> pd.DataFrame:
-    if db_conn is None:
-        db_conn = sqlite3.connect('/home/orestis/code/Orestis/InvestorAPI/app/database/ibd.db')
-
-    query = '''
-        SELECT * 
-        FROM price_prediction_dataset 
-        WHERE DATE(fiscal_date_ending) <= date('now', '-3 months')
-        ORDER BY DATE(fiscal_date_ending)
-    '''
-
-    stocks_df = pd.read_sql(query, db_conn)
-    db_conn.close()
-
-    # Drop rows that contain null values
-    columns_with_null = stocks_df.columns[stocks_df.isna().any()].tolist()
-    stocks_df.dropna(subset=columns_with_null, inplace=True)
-    stocks_df.reset_index(inplace=True, drop=True)
-
-    world_indices_value_change_cols = [
-        's_p_500_index_value_change',
-        'dow_jones_index_value_change',
-        'nasdaq_index_value_change',
-        'nyse_index_value_change'
-    ]
-
-    economic_indicators_avg_value_cols = [
-        'avg_interest_rate',
-        'avg_treasury_yield',
-        'avg_natural_gas_price',
-        'avg_oil_price',
-        'avg_unemployment_rate',
-        'avg_global_commodities_index_value',
-        'inflation'
-    ]
-
-    economic_indicators_value_change_cols = [
-        'interest_rate_value_change',
-        'treasury_yield_value_change',
-        'natural_gas_price_value_change',
-        'oil_price_value_change',
-        'unemployment_rate_value_change',
-        'global_commodities_index_value_change',
-    ]
-
-
-    value_change_cols = ['change_in_cash_and_cash_equivalents', 'change_in_exchange_rate']
-    for col_name in stocks_df.columns:
-        if '_value_change' in col_name:
-            if col_name not in world_indices_value_change_cols and col_name not in economic_indicators_value_change_cols:
-                value_change_cols.append(col_name)
-
-
-    columnss_to_keep = value_change_cols + economic_indicators_avg_value_cols + ['symbol', 'fiscal_date_ending', 'sector', 'avg_three_months_price', 'avg_next_three_months_price']
-
-    return stocks_df[columnss_to_keep]
+    return stock_df
 
 
 def split_data_to_train_and_test(
@@ -168,92 +103,6 @@ def split_data_to_train_and_test(
     test_set = test_set.reset_index(drop=True)
 
     return train_set, test_set 
-
-
-def split_into_input_and_target(
-    train_set: pd.DataFrame,
-    test_set: pd.DataFrame
-) -> Tuple[pd.DataFrame]:
-    """
-    Returns (X_train, y_train, X_test, y_test)
-    """
-    y_train = train_set['price']
-    X_train = train_set.drop(['price'], axis=1)
-
-    y_test = test_set['price']
-    X_test = test_set.drop(['price'], axis=1)
-
-    return (X_train, y_train, X_test, y_test)
-
-
-def transform_input(
-    X: pd.DataFrame,
-    one_hot_encoder: OneHotEncoder,
-    scaler: Optional[MinMaxScaler | StandardScaler] = None,
-    fit: bool = False
-) -> pd.DataFrame:
-    """
-    Performs one hot encoding and min max scaling
-    on X input set. Parameter fit should be True 
-    when X param is the training set.
-    """
-    # Drop useless columns
-    X.drop(['symbol', 'fiscal_date_ending', 'reported_currency'], axis=1, inplace=True)
-    
-    one_hot_encoded_train_set = preprocessing.perform_one_hot_encoding(
-        df=X,
-        categorical_columns=['sector'],
-        encoder=one_hot_encoder,
-        fit=fit
-    )
-
-    if scaler:
-        float_columns = X.select_dtypes(include=['float64'])
-        columns_to_scale = list(float_columns.columns)
-
-        scaled_train_set = preprocessing.perform_scaling(
-            df=one_hot_encoded_train_set,
-            scaler=scaler,
-            fit=True,
-            columns_to_scale=columns_to_scale
-        )
-        return scaled_train_set.reset_index(drop=True)
-
-    return one_hot_encoded_train_set
-
-
-def tranform_target(
-    y: pd.Series,
-    scaler: MinMaxScaler,
-    inverse: bool = False,
-    fit: bool = False
-) -> pd.Series:
-    """
-    Performs min max scaling on y target.
-    Parameter fit should be True if target is
-    used for training otherwise False.
-    Parameter inverse should be True in case
-    that we want to transform the scaled value back
-    to it's normal scale 
-    """
-    return preprocessing.scale_transformation_on_target(
-        target=y,
-        scaler=scaler,
-        inverse=inverse,
-        fit=fit
-    )
-
-
-def calculate_avg_pct_loss(
-    y_pred: pd.Series,
-    y_actual: pd.Series
-) -> float:
-    total_pct_loss = 0
-    for i in range(len(y_pred)):
-        pct_loss = (abs(y_pred[i] - y_actual[i]) / y_pred[i]) * 100
-        total_pct_loss += pct_loss
-    
-    return float(total_pct_loss / len(y_pred))
 
 
 def calculate_avg_pct_loss_per_sector(
@@ -281,48 +130,6 @@ def calculate_avg_pct_loss_per_sector(
         )
 
     return avg_pct_loss_per_sector
-
-
-def calculate_avg_pct_loss_per_price_bucket(
-    y_pred: pd.Series,
-    y_actual: pd.Series,
-    buckets: List[Tuple[float, float]]
-):
-    def get_price_bucket(price: float) -> int:
-        """
-        Returns the bucket index that the price belongs to or
-        -1 in case the price doesn't belong to any bucket
-        """
-        for i in range(len(buckets)):
-            if price > buckets[i][0] and price < buckets[i][1]:
-                return i
-
-        return -1
-
-    def create_string_key_for_bucket(bucket_index: int) -> str:
-        if bucket_index == -1:
-            return f"{buckets[bucket_index][1]}+"
-
-        return f"{buckets[bucket_index][0]} - {buckets[bucket_index][1]}"
-
-    bucket_total_pct_loss_dict = dict()
-    bucket_pred_count_dict = dict()
-    for i in range(len(y_pred)):
-        pct_loss = (abs(y_pred[i] - y_actual[i]) / y_pred[i]) * 100
-        bucket_index = get_price_bucket(y_actual[i])
-        bucket_name = create_string_key_for_bucket(bucket_index)
-        if bucket_name in bucket_total_pct_loss_dict:
-            bucket_total_pct_loss_dict[bucket_name] += pct_loss
-            bucket_pred_count_dict[bucket_name] += 1
-        else:
-            bucket_total_pct_loss_dict[bucket_name] = pct_loss
-            bucket_pred_count_dict[bucket_name] = 1
-    
-    avg_pct_loss_per_bucket_dict = dict() 
-    for bucket in bucket_total_pct_loss_dict:
-        avg_pct_loss_per_bucket_dict[bucket] = bucket_total_pct_loss_dict[bucket] / bucket_pred_count_dict[bucket]
-
-    return avg_pct_loss_per_bucket_dict
 
 
 def get_feature_importance_sorted(
---
+++ analytics/utils.py
@@ -0,0 +1,216 @@
+import datetime as dt
+import sqlite3
+from typing import Optional
+
+import pandas as pd
+
+def get_interest_rate_df(conn: sqlite3.Connection) -> pd.DataFrame:
+    query = '''
+    SELECT  *
+    FROM economic_indicator_time_series
+    WHERE indicator_name = 'Interest_Rate'
+    '''
+
+    interest_rate_df = pd.read_sql(query, conn)
+    return interest_rate_df
+
+
+def get_treasury_yield_df(conn: sqlite3.Connection) -> pd.DataFrame:
+    query = '''
+    SELECT  *
+    FROM economic_indicator_time_series
+    WHERE indicator_name = 'Treasury_Yield'
+    '''
+
+    treasury_yield_df = pd.read_sql(query, conn)
+    return treasury_yield_df
+
+
+def get_stock_time_series_df(symbol: str, conn: sqlite3.Connection) -> pd.DataFrame:
+    query = f'''
+    SELECT  *
+    FROM stock_time_series
+    WHERE symbol = '{symbol}'
+    ORDER BY registered_date_ts DESC
+    '''
+    
+    stock_time_series_df = pd.read_sql(query, conn)
+    return stock_time_series_df
+
+
+def get_sector_time_series_df(sector: str, conn: sqlite3.Connection) -> pd.DataFrame:
+    query = f'''
+    SELECT AVG(sts.close_price) AS sector_price, substr(sts.registered_date, 4, 7) AS month_year
+    FROM stock_time_series as sts
+    INNER JOIN stock_overview as so
+    ON sts.symbol = so.symbol
+    WHERE so.sector = '{sector}'
+    GROUP BY month_year
+    ORDER BY sts.registered_date_ts ASC
+    '''
+
+    sector_df = pd.read_sql(query, conn)
+    sector_df['Date'] = pd.to_datetime(sector_df['month_year'], format='%m-%Y')
+    return sector_df
+
+
+def calculate_time_series_pct_change(
+    start_date: str,
+    time_series_df: pd.DataFrame,
+    target_column: str,
+    days: int
+) -> Optional[int]:
+    """
+    Given a start calculate what was the pct change
+    between <start_date> and <start_date> + <days> time
+    """
+    if days < 0:
+        lower_bound = pd.Timestamp(start_date) - pd.DateOffset(days=abs(days))
+        upper_bound = pd.Timestamp(start_date) 
+    else:
+        lower_bound = pd.Timestamp(start_date)
+        upper_bound = lower_bound + pd.DateOffset(days=days)
+    
+    time_series_df['registered_date_ts'] = pd.to_datetime(time_series_df['registered_date_ts'], unit='s')
+    # Filter the DataFrame
+    filtered_df = time_series_df[
+        (time_series_df['registered_date_ts'] >= lower_bound) & 
+        (time_series_df['registered_date_ts'] <= upper_bound)
+    ]
+
+    if len(filtered_df) == 0:
+        return None
+
+    # Sort the filtered DataFrame by timestamp
+    filtered_df = filtered_df.sort_values(by='registered_date_ts')
+
+    # Calculate pct change between first and last row
+    pct_change = ((filtered_df[target_column].iloc[-1] - filtered_df[target_column].iloc[0]) / filtered_df[target_column].iloc[0])
+    return pct_change
+
+
+def calculate_sector_pct_change(
+    start_date: dt.datetime,
+    time_series_df: pd.DataFrame,
+    days: int
+) -> Optional[int]:
+    """
+    Given a start calculate what was the pct change
+    between <start_date> and <start_date> +/- <days> time
+    """
+    if days < 0:
+        lower_bound = start_date - pd.DateOffset(days=abs(days))
+        upper_bound = start_date 
+    else:
+        lower_bound = start_date
+        upper_bound = lower_bound + pd.DateOffset(days=days)
+    
+    # Filter the DataFrame
+    filtered_df = time_series_df[
+        (time_series_df['Date'] >= lower_bound) & 
+        (time_series_df['Date'] <= upper_bound)
+    ]
+
+    if len(filtered_df) == 0:
+        return None
+
+    # Sort the filtered DataFrame by timestamp
+    filtered_df = filtered_df.sort_values(by='Date')
+
+    # Calculate pct change between first and last row
+    pct_change = ((filtered_df['sector_price'].iloc[-1] - filtered_df['sector_price'].iloc[0]) / filtered_df['sector_price'].iloc[0])
+    return pct_change
+
+
+def find_time_series_most_recent_value(
+    start_date: str,
+    time_series_df: pd.DataFrame,
+    target_column: str,
+    days: int 
+) -> Optional[int]:
+    """
+    Given a start_date find the most recent value
+    between <start_date> and <start_date> +/- <days> time
+    """
+    if days < 0:
+        lower_bound = pd.Timestamp(start_date) - pd.DateOffset(days=abs(days))
+        upper_bound = pd.Timestamp(start_date) 
+    else:
+        lower_bound = pd.Timestamp(start_date)
+        upper_bound = lower_bound + pd.DateOffset(days=days)
+    
+    time_series_df['registered_date_ts'] = pd.to_datetime(time_series_df['registered_date_ts'], unit='s')
+    # Filter the DataFrame
+    filtered_df = time_series_df[
+        (time_series_df['registered_date_ts'] >= lower_bound) & 
+        (time_series_df['registered_date_ts'] <= upper_bound)
+    ]
+
+    if len(filtered_df) == 0:
+        return None
+
+    # Sort the filtered DataFrame by timestamp
+    filtered_df = filtered_df.sort_values(by='registered_date_ts')
+    return filtered_df[target_column].iloc[-1]
+
+
+def calculate_time_series_volatility(
+    start_date: str,
+    time_series_df: pd.DataFrame,
+    target_column: str,
+    days: int
+) -> Optional[int]:
+    """
+    Given a start calculate what was the volatility
+    between <start_date> and <start_date> +/- <days> time
+    """
+    if days < 0:
+        lower_bound = pd.Timestamp(start_date) - pd.DateOffset(days=abs(days))
+        upper_bound = pd.Timestamp(start_date) 
+    else:
+        lower_bound = pd.Timestamp(start_date)
+        upper_bound = lower_bound + pd.DateOffset(days=days)
+    
+    time_series_df['registered_date_ts'] = pd.to_datetime(time_series_df['registered_date_ts'], unit='s')
+    # Filter the DataFrame
+    filtered_df = time_series_df[
+        (time_series_df['registered_date_ts'] >= lower_bound) & 
+        (time_series_df['registered_date_ts'] <= upper_bound)
+    ]
+
+    if len(filtered_df) == 0:
+        return None
+
+    # Sort the filtered DataFrame by timestamp
+    filtered_df = filtered_df.sort_values(by='registered_date_ts')
+
+    volatility = filtered_df[target_column].pct_change().std()
+    return volatility
+
+
+def find_latest_financials_data(
+    start_date,
+    financials_time_series_df: pd.DataFrame,
+    days: int = 30 * 6
+):
+    """
+    Returns the most recent financials data of a stock from 'start_date'(Going backwards)
+    """
+    upper_bound = start_date
+    lower_bound = start_date - pd.DateOffset(days=days)
+    
+    financials_time_series_df['fiscal_date_ending'] = pd.to_datetime(financials_time_series_df['fiscal_date_ending'], format='%Y-%m-%d')
+    # Filter the DataFrame
+    filtered_df = financials_time_series_df[
+        (financials_time_series_df['fiscal_date_ending'] >= lower_bound) & 
+        (financials_time_series_df['fiscal_date_ending'] <= upper_bound)
+    ]
+
+    if len(filtered_df) == 0:
+        return None
+
+    # Sort the filtered DataFrame by timestamp
+    filtered_df = filtered_df.sort_values(by='fiscal_date_ending')
+
+    columns_to_return = [col_name for col_name in filtered_df.columns if str(col_name).endswith('_arctan_pct_change')]    
+    return filtered_df[columns_to_return].iloc[-1]
