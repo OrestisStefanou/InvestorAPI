1cdcf5c50356c11ac6d324a00c11d247f336c49f
OrestisStefanou <orestaras1997@outlook.com>
2024-01-03 15:54:18 +0200

    Base predictor class

---
+++ analytics/machine_learning/price_prediction_with_fundamentals/ml_models/six_months_price_predictor.py
@@ -0,0 +1,7 @@
+import joblib
+
+from analytics.machine_learning.price_prediction_with_fundamentals.ml_models.base_predictor import PriceMovementPredictor
+
+
+class SixMonthsPriceMovementPredictor(PriceMovementPredictor):
+    _ml_model = joblib.load('/Users/orestis/MyProjects/InvestorAPI/analytics/machine_learning/price_prediction_with_fundamentals/ml_models/rf_six_months_prediction_model.joblib')
