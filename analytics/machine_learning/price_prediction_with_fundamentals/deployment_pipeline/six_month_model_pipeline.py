eab8e85a5426b6fc5e80797beb17a6e46092e415
OrestisStefanou <orestaras1997@outlook.com>
2024-01-28 18:14:36 +0200

    Update six months model thresholds

--- analytics/machine_learning/price_prediction_with_fundamentals/deployment_pipeline/six_month_model_pipeline.py
+++ analytics/machine_learning/price_prediction_with_fundamentals/deployment_pipeline/six_month_model_pipeline.py
@@ -6,6 +6,6 @@ class SixMonthModelPipeline(BaseModelPipeline):
             months=6,
             target_price_column='price_pct_change_next_six_months', 
             current_model_path='/Users/orestis/MyProjects/InvestorAPI/analytics/machine_learning/price_prediction_with_fundamentals/ml_models/rf_six_months_prediction_model.joblib',
-            true_negative_threshold=0.85,
-            true_positive_threshold=0.85
+            true_negative_threshold=0.65,
+            true_positive_threshold=0.65
         )
